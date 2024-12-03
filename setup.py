9ee8004d22983eb814fccaf4a34a6fdb02ec2fbe
rmr327 <rmr62@rmr62>
2024-12-03 12:27:01 -0500

    setup chnges

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="LlmEmbeddingXrVizualization",
-    version="0.1.0",
+    version="0.1.1",
     author="Rakeen Rouf & Akalpit",
     author_email="rakeenrouf@gmail.com",
     description="A package for visualizing embeddings spaces from Hugging Face models",
